fb380db591471fe74acbd69547b672588714803f
baluns369 <baluns2003@gmail.com>
2024-07-21 23:36:13 +0530

    done

---
+++ js/exp.js
@@ -0,0 +1,11 @@
+let ar=[1,"vini",true,null,false,"hala",7]
+console.log(typeof(ar))
+console.log(ar.length)
+console.log(ar[1])
+console.log(ar.indexOf("hala"))
+ar.push(10)
+// console.log(ar)
+// console.log(ar.pop())
+// console.log(ar.shift())
+// console.log(ar[1]="ney")
+console.log(ar[ar.indexOf("hala")="suiii"])
